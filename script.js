89f76ba5cd3cf9f7d798627dd6fe85d713996947
sambhavgupta0705 <81870866+sambhavgupta0705@users.noreply.github.com>
2022-07-23 08:21:42 +0530

    Update script.js
    Added the new Api_Key
    The previous one had some error in it.

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const currentTempEl = document.getElementById('current-temp');
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-const API_KEY ='49cc8c821cd2aff9af04c9f98c36eb74';
+const API_KEY ='d803ac7bc746b7762b6d6722a26c5c85';
 
 setInterval(() => {
     const time = new Date();
